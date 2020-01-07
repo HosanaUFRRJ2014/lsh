23af51d50ba5e177f618747f58764c1cfeb6b2b2
Hosana Gomes <hosannagomes@hotmail.com>
2020-01-07 02:31:09 -0300

    Jaccard similarity and ranking

--- lsh.py
+++ lsh.py
@@ -1,9 +1,7 @@
 # -*-coding:utf8;-*-
-# qpy:3
-# qpy:console
-from random import sample, randint
 from copy import copy
 import numpy as np
+from random import sample, randint
 from pprint import pprint
 from scipy.sparse import lil_matrix
 
@@ -19,6 +17,15 @@ SELECTION_FUNCTIONS = [
     max
 ]
 SELECTION_FUNCTION_COUNT = len(SELECTION_FUNCTIONS)
+
+
+def get_document_chunks(document):
+    '''
+    Split the document in chunks.
+    '''
+    if isinstance(document, list):
+        document = document[0]
+    return document.split(' ')
 
 
 def _get_index(permutation_number, selecion_function_code=0):
@@ -40,18 +47,15 @@ def tokenize(documents, v):
     td_matrix_temp = []
     for i in range(len(documents)):
         di_terms = []
-        for termj in documents[i].split(" "):
+        document_chunks = get_document_chunks(documents[i])
+        for termj in document_chunks:
             di_terms.append(vocab_index(termj, v))
         td_matrix_temp.append(di_terms)
         di_terms = None
     td_matrix = np.zeros([len(v), len(documents)])
-    # print(td_matrix.shape)
+
     for i in range(len(documents)):
         td_matrix[np.array(td_matrix_temp[i]) - 1, i] = 1
-
-        # print(documents[i])
-        # print(td_matrix)
-        # print('='*10)
 
     del td_matrix_temp
     td_matrix_temp = None
@@ -71,18 +75,7 @@ def permutate(to_permute, shuffle_seed, fingerprints):
     return resultant_fingerprints
 
 
-def generate_permutations(to_permute, number_of_permutations, fingerprints):
-    # TODO: Improve it! Looks poor
-    permutations = [
-        permutate(to_permute, i, fingerprints)
-        for i in range(number_of_permutations + 1)
-    ]
-    return permutations
-
-
-def generate_inverted_index(
-    documents, td_matrix, permutation_count
-):
+def generate_inverted_index(td_matrix, permutation_count):
     # num_lines = permutation_count * (SELECTION_FUNCTION_COUNT + 1) + SELECTION_FUNCTION_COUNT
     num_lines = permutation_count * SELECTION_FUNCTION_COUNT  # + SELECTION_FUNCTION_COUNT
     num_columns = td_matrix.shape[0] #+ 1
@@ -91,7 +84,7 @@ def generate_inverted_index(
         dtype=np.ndarray
     )
 
-    fingerprints = np.array(range(1, num_columns+1))
+    fingerprints = np.array(range(1, num_columns + 1))
     for j in range(td_matrix.shape[1]):
         for i in range(permutation_count):
             dj_permutation = permutate(
@@ -104,9 +97,11 @@ def generate_inverted_index(
                     permutation_number=i,
                     selecion_function_code=l
                 )
-                X = np.nonzero(dj_permutation)
-                second_index = int(SELECTION_FUNCTIONS[l](dj_permutation[X]))-1
-                print("(%d, %d) on (%d, %d)"%(first_index, second_index, num_lines, num_columns),dj_permutation[X])
+                non_zero_indexes = np.nonzero(dj_permutation)
+                second_index = int(
+                    SELECTION_FUNCTIONS[l](dj_permutation[non_zero_indexes])
+                ) - 1
+                print("(%d, %d) on (%d, %d)"%(first_index, second_index, num_lines, num_columns),dj_permutation[non_zero_indexes])
                 if isinstance(inverted_index[first_index][second_index], np.ndarray):
                     inverted_index[first_index][second_index] = np.append(
                         inverted_index[first_index][second_index],
@@ -123,11 +118,11 @@ def search_inverted_index(
     query_td_matrix, inverted_index, permutation_count
 ):
     # num_lines = permutation_count * (SELECTION_FUNCTION_COUNT + 1) + SELECTION_FUNCTION_COUNT
-    documents_rank = np.zeros((inverted_index.shape[1] + 1, ), dtype=int)
+    similar_docs_count = np.zeros((inverted_index.shape[1] + 1, ), dtype=int)
     num_lines = permutation_count * SELECTION_FUNCTION_COUNT
     num_columns = query_td_matrix.shape[0] # + 1
 
-    fingerprints = np.array(range(1, num_columns+1))
+    fingerprints = np.array(range(1, num_columns + 1))
     for j in range(query_td_matrix.shape[1]):
         for i in range(permutation_count):
             dj_permutation = permutate(
@@ -140,38 +135,85 @@ def search_inverted_index(
                     permutation_number=i,
                     selecion_function_code=l
                 )
-                X = np.nonzero(dj_permutation)
-                second_index = int(SELECTION_FUNCTIONS[l](dj_permutation[X]))-1
+                non_zero_indexes = np.nonzero(dj_permutation)
+                second_index = int(
+                    SELECTION_FUNCTIONS[l](dj_permutation[non_zero_indexes])
+                ) - 1
 
                 try:
                     retrieved_documents = inverted_index[first_index][second_index]
-
-                    if retrieved_documents != 0:
-                        documents_rank[retrieved_documents] += 1
+                    print('retrieved_documents:', retrieved_documents)
+                    if not isinstance(retrieved_documents, int):
+                        similar_docs_count[retrieved_documents] += 1
                     print("retrieved documents for fingerprint %d : "%(second_index), retrieved_documents)
                 except IndexError as e:
                     continue
-    return documents_rank
+    print('similar_docs_count: ', similar_docs_count)
+    return similar_docs_count
+
+
+def calculate_jaccard_similarity(query_document, similar_document):
+    '''
+    Jaccard Similarity algorithm in steps:
+    1. Get the shared members between both sets, i.e. intersection.
+    2. Get the members in both sets (shared and un-shared, i.e. union).
+    3. Divide the number of shared members found in (1) by the total number of
+       members found in (2).
+    4. Multiply the found number in (3) by 100.
+    '''
+    query_chunks = get_document_chunks(query_document)
+    similar_document_chunks = get_document_chunks(similar_document)
+
+    intersection = set(similar_document_chunks).intersection(query_chunks)
+    union = set(similar_document_chunks + query_chunks)
+
+    jaccard_similarity = len(intersection) / len(union) * 100
+
+    return jaccard_similarity
+
+
+def calculate_jaccard_similarities(query_document, similar_docs_count, documents):
+    '''
+    Calculates jaccard similarity for all similar documents found in lsh search.
+    Return an ordered jaccard similarity dictionary from de most to the less
+    similar document.
+    '''
+    similar_docs_indexes = (np.nonzero(similar_docs_count)[0] - 1)
+    similar_documents = documents[similar_docs_indexes]
+    jaccard_similarities = {
+        doc_index + 1: calculate_jaccard_similarity(query_document, document)
+        for doc_index, document in zip(similar_docs_indexes, similar_documents)
+    }
+    jaccard_similarities = sorted(
+        jaccard_similarities.items(),
+        key=lambda sim: sim[1],
+        reverse=True
+    )
+    return jaccard_similarities
 
 
 def main():
-    NUM_OF_PERMUTATIONS = 4
-    documents = [d1, d2, d3, d4, d5]
+    NUM_OF_PERMUTATIONS = 4  # aumentar para testar com dataset maior
+    documents = np.array([d1, d2, d3, d4, d5])
     vocabulary = {}
     td_matrix = tokenize(documents, vocabulary)
     print(td_matrix)
     print(vocabulary)
 
-    inverted_index = generate_inverted_index(
-        documents, td_matrix, NUM_OF_PERMUTATIONS
-    )
+    inverted_index = generate_inverted_index(td_matrix, NUM_OF_PERMUTATIONS)
 
-    query = ['paralelepipedoebacana é']
+    query = ['Hosana Gomes']
     query_td_matrix = tokenize(query, vocabulary)
-    documents_rank = search_inverted_index(
+    similar_docs_count = search_inverted_index(
         query_td_matrix, inverted_index, NUM_OF_PERMUTATIONS
     )
-    print(documents_rank)
+    jaccard_similarities = calculate_jaccard_similarities(
+        query, similar_docs_count, documents
+    )
+
+    for doc_index, sim_percent in jaccard_similarities:
+        print('document {} is {}% similar'.format(doc_index, sim_percent))
+    print('jaccard_similarities: ', jaccard_similarities)
 
 
 if __name__ == '__main__':
