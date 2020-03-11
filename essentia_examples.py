0056a51259ead9deee476f9e1d3d6361ead2d11d
Hosana Gomes <hosannagomes@hotmail.com>
2020-03-11 03:32:33 -0300

    Pitch extraction draft

---
+++ essentia_examples.py
@@ -0,0 +1,51 @@
+# Essentia usage examples
+
+filepath = '/home/hosana/TCC/uniformiza_dataset/songs_wav/000002.wav'
+
+#
+# # Music feature extraction
+from essentia.standard import MusicExtractor, EqloudLoader, PredominantPitchMelodia
+from math import floor
+
+my_extractor = MusicExtractor()
+features, features_frames = my_extractor.compute(filepath)
+
+features_keys = features.descriptorNames()
+# for key in features_keys:
+    # print('{}: {}'.format(key, features[key]))
+    # print('{}'.format(key))
+
+#
+# # Pitch Extraction
+
+# Loads the song
+loader = EqloudLoader(filename=filepath, sampleRate=44100)
+audio = loader()
+print("Duration of the audio sample [sec]:")
+print(len(audio)/44100.0)
+
+# Extract the pitch curve
+# PitchMelodia takes the entire audio signal as input (no frame-wise processing is required)
+
+pitch_extractor = PredominantPitchMelodia(frameSize=2048, hopSize=128)
+pitch_values, pitch_confidence = pitch_extractor(audio)
+
+print('Pitch values: {}'.format(pitch_values))
+
+# One song in PLSH index
+EXTRACTING_INTERVAL = 2
+WINDOW_SHIFT = 15
+WINDOW_LENGTH = 60
+pitch_vectors = []
+window_start = 0
+number_of_windows = len(pitch_values) / (WINDOW_SHIFT)
+number_of_windows = floor(number_of_windows)
+for window in range(number_of_windows):
+    window_end = window_start + WINDOW_LENGTH
+    pitch_vector = pitch_values[window_start:window_end:EXTRACTING_INTERVAL]
+    pitch_vectors.append(pitch_vector)
+    window_start += WINDOW_SHIFT
+
+# need to build
+# another index to record the position of the vectors in
+# the original melody
