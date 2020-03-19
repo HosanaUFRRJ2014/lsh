9b53ed9304021c2e2f967b615a1423d7cd025536
Hosana Gomes <hosannagomes@hotmail.com>
2020-03-19 04:35:11 -0300

    Implements load queries + code adjustments

--- constants.py
+++ constants.py
@@ -7,6 +7,9 @@ SELECTION_FUNCTIONS = [
 SELECTION_FUNCTION_COUNT = len(SELECTION_FUNCTIONS)
 
 
-PATH_TO_DATASET = '/home/hosana/TCC/uniformiza_dataset/'
-WAV_SONGS_FILENAME = 'wav_songs.list'
-SONGS_WAV_PATH = 'songs_wav'
+PATH_TO_DATASET = '../uniformiza_dataset'
+FILENAMES_OF_SONGS = '{}/{}'.format(PATH_TO_DATASET, 'wav_songs.list')
+WAV_SONGS_PATH = 'songs_wav'
+
+FILENAMES_OF_QUERIES = '{}/{}'.format(PATH_TO_DATASET, 'queries.list')
+QUERIES_PATH = 'queries'
--- loader.py
+++ loader.py
@@ -3,26 +3,30 @@ from essentia.standard import (
     EqloudLoader,
     PredominantPitchMelodia
 )
-from constants import PATH_TO_DATASET, WAV_SONGS_FILENAME, SONGS_WAV_PATH
+from constants import (
+    FILENAMES_OF_SONGS,
+    PATH_TO_DATASET,
+    WAV_SONGS_PATH,
+    FILENAMES_OF_QUERIES,
+    QUERIES_PATH
+)
 
 Extractor = MusicExtractor()
-pitch_extractor = PredominantPitchMelodia(frameSize=2048, hopSize=128)
 
-__all__ = ["load_all_songs_pitch_vectors"]
+__all__ = ["load_all_songs_pitch_vectors", "load_all_queries_pitch_vectors"]
 
 
-def _read_songs_names():
-    path = '{}{}'.format(PATH_TO_DATASET, WAV_SONGS_FILENAME)
-    file = open(path, 'r')
-    songs_wav_paths = [
-        '{}/{}'.format(SONGS_WAV_PATH, name.rstrip('\n'))
-        for name in file.readlines()
+def _read_dataset_names(path, audio_path):
+    filenames_file = open(path, 'r')
+    paths = [
+        '{}/{}/{}'.format(PATH_TO_DATASET, audio_path, name.rstrip('\n'))
+        for name in filenames_file.readlines()
     ]
-    file.close()
-    return songs_wav_paths
+    filenames_file.close()
+    return paths
 
 
-def _load_song(filepath):
+def _load_audio(filepath):
     # TODO: Calculate some features in other function, not all of them
     # features, features_frames = Extractor.compute(filepath)
     # features_keys = features.descriptorNames()
@@ -34,23 +38,26 @@ def _load_song(filepath):
 
 
 def _extract_pitch_vector(audio):
+    pitch_extractor = PredominantPitchMelodia(frameSize=2048, hopSize=128)
     pitch_values, _pitch_confidence = pitch_extractor(audio)
     return pitch_values
 
 
-def load_all_songs_pitch_vectors():
-    '''
-    Time:
-    real    2m54,734s
-    user    2m22,878s
-    sys     0m15,550s
-    '''
+def _load_all_audio_pitch_vectors(filenames_file, path):
     pitch_vectors = []
-    songs_paths = _read_songs_names()
-    for song_path in songs_paths[:10]:
-        filepath = '{}{}'.format(PATH_TO_DATASET, song_path)
-        audio = _load_song(filepath)
+    audios_paths = _read_dataset_names(filenames_file, path)
+    for audio_path in audios_paths[:16]:
+        print('path: ', audio_path)
+        audio = _load_audio(audio_path)
         pitch_vector = _extract_pitch_vector(audio)
         pitch_vectors.append(pitch_vector)
 
     return pitch_vectors
+
+
+def load_all_songs_pitch_vectors():
+    return _load_all_audio_pitch_vectors(FILENAMES_OF_SONGS, WAV_SONGS_PATH)
+
+
+def load_all_queries_pitch_vectors():
+    return _load_all_audio_pitch_vectors(FILENAMES_OF_QUERIES, QUERIES_PATH)
